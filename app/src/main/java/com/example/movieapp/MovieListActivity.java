ac968287b1570ad2415a1af862b8308dd5eac7ad
Ashwin <ashwingur88@gmail.com>
2021-07-08 23:18:19 +1000

    Made response class

---
+++ app/src/main/java/com/example/movieapp/MovieListActivity.java
@@ -0,0 +1,32 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getRetrofitResponse();
+            }
+        });
+    }
+
+    private void getRetrofitResponse() {
+        
+
+    }
+}
