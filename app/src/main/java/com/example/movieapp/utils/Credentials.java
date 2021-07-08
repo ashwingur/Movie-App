8be45a47f42b6e5ac7c1ac365aa786328ba4ca38
Ashwin <ashwingur88@gmail.com>
2021-07-08 22:14:15 +1000

    Added retrofit dependency, created credentials class

---
+++ app/src/main/java/com/example/movieapp/utils/Credentials.java
@@ -0,0 +1,9 @@
+package com.example.movieapp.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String API_KEY = "e8932bf1e7716def8785f719d9ca7168";
+
+
+}
