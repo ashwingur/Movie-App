0aceafe0348183adcff47ce64170b35b399b8e2e
Ashwin <ashwingur88@gmail.com>
2021-07-08 23:06:22 +1000

    Created MovieModel, Servicee, MovieReponse, MovieSearchResponse, MovieApi

---
+++ app/src/main/java/com/example/movieapp/request/Servicee.java
@@ -0,0 +1,23 @@
+package com.example.movieapp.request;
+
+import com.example.movieapp.utils.Credentials;
+import com.example.movieapp.utils.MovieApi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Servicee {
+
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+            .baseUrl(Credentials.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    private static MovieApi movieApi = retrofit.create(MovieApi.class);
+
+    public MovieApi getMovieApi(){
+        return movieApi;
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/response/MovieSearchResponse.java
@@ -0,0 +1,4 @@
+package com.example.movieapp.response;
+
+public class MovieSearchResponse {
+}
